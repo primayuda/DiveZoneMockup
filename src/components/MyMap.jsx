d60a9d0db436f70ad24df44f55064ffbc9fba462
Aprita Primayuda <80294439+primayuda@users.noreply.github.com>
2022-09-24 04:54:34 +0000

    Adding marker, popup and collect cities data on cities.js

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,13 +1,41 @@
-import Map, {Marker, NavigationControl} from 'react-map-gl';
+import { useState, useMemo } from 'react';
+import Map, {Marker, NavigationControl, Popup } from 'react-map-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const MAPBOX_TOKEN = 'pk.eyJ1IjoicHJpbWF5dWRhIiwiYSI6ImNrcGNicG1qejE0emYydW11NmpqZXAwZHMifQ.0XbWGmeEjExM9Ol0M-Dg7Q'; // Set your mapbox token here
+import Pin from './Pins';
+
+import CITIES from '../data/cities';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoicHJpbWF5dWRhIiwiYSI6ImNsODlxMXI4YzA5cG0zbnFrZ3dmdWhsYTYifQ.UMHawW_6NTEZN1Qv026xwg'; // Set your mapbox token here
 
 export default function MyMap() {
+  const [popupInfo, setPopupInfo] = useState(null);
+
+  const pins = useMemo(
+    () =>
+      CITIES.map((city, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={city.longitude}
+          latitude={city.latitude}
+          anchor="bottom"
+          onClick={e => {
+            // If we let the click event propagates to the map, it will immediately close the popup
+            // with `closeOnClick: true`
+            e.originalEvent.stopPropagation();
+            setPopupInfo(city);
+          }}
+        >
+          <Pin />
+        </Marker>
+      )),
+    []
+  );
+
   return (
     <Map
-      initialViewState={{   // center on Denpasar
+      initialViewState={{   // center on Indonesia
         latitude: -2,
         longitude: 115.21,
         zoom: 4.5
@@ -16,9 +44,28 @@ export default function MyMap() {
       mapStyle="mapbox://styles/mapbox/outdoors-v11"
       mapboxAccessToken={MAPBOX_TOKEN}
     >
-      <NavigationControl />
-      <Marker longitude={106.81} latitude={-6.2} color="red" />  // Jakarta
-      <Marker longitude={115.22} latitude={-8.65} color="red" />  // Jakarta
+      <NavigationControl position='top-left' />
+      {pins}
+
+      {popupInfo && (
+          <Popup
+            anchor="bottom"
+            longitude={Number(popupInfo.longitude)}
+            latitude={Number(popupInfo.latitude)}
+            onClose={() => setPopupInfo(null)}
+          >
+            <div>
+              {popupInfo.city}, {popupInfo.state} |{' '}
+              <a
+                target="_new"
+                href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
+              >
+                Wikipedia
+              </a>
+            </div>
+            <img width="100%" src={popupInfo.image} />
+          </Popup>
+        )}
     </Map>
   );
 }
---
+++ src/data/cities.js
@@ -0,0 +1,7 @@
+const CITIES = [
+  {"city":"Jakarta","population":"8,175,133","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Merdeka_Square_Monas_02.jpg/800px-Merdeka_Square_Monas_02.jpg","state":"DKI Jaya","latitude":-6.2,"longitude":106.81},
+  {"city":"Denpasar","population":"3,792,621","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Odalan_procession.JPG/1920px-Odalan_procession.JPG","state":"Bali","latitude":-8.65,"longitude":115.22},
+  
+]
+
+export default CITIES;
